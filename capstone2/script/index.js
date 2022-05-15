ee3389745022a4d45faf54ea04a5edc899a9426b
IGNORAMUS1 <104022564+IGNORAMUS1@users.noreply.github.com>
2022-05-15 11:05:03 +0100

    Responsive hamburger icon

---
+++ capstone2/script/index.js
@@ -0,0 +1,9 @@
+//for responsiveness of the hamburger icon
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
