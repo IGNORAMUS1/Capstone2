ed6d385f7e45ed12b9561acd96aba06952518b2c
Emmanuel Akinbo <59098948+akinbo-emmanuel@users.noreply.github.com>
2022-05-16 12:52:41 +0100

    Repositioning files

--- capstone2/script/index.js
+++
@@ -1,9 +0,0 @@
-//for responsiveness of the hamburger icon
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
