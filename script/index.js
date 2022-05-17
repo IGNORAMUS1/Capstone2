6f932bd289d67899252f1712df0110c781431528
Emmanuel Akinbo <59098948+akinbo-emmanuel@users.noreply.github.com>
2022-05-17 11:23:14 +0100

    Semi-final update

--- script/index.js
+++ script/index.js
@@ -1,9 +1,24 @@
 //for responsiveness of the hamburger icon
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } 
+  else {
+    x.className = "topnav";
   }
+}
+
+let password = document.getElementById("validationCustom03"), confirm_password = document.getElementById("validationCustom04"), confirmPasswordFeedback = document.getElementById("confirmPasswordFeedback");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirmPasswordFeedback.innerHTML = "The passwords are not alike";
+  } 
+  else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
